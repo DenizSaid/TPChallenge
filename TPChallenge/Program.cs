d6758a5b1e97f920644fe50102ce77f8fc57d032
Deniz Said <deniz@dsitmanagement.com>
2016-06-22 21:47:39 +0200

    Added wordlist to projedt and created function to read the wordlist as an embeded resource

--- TPChallenge/Program.cs
+++ TPChallenge/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace TPChallenge
 {
     class Program
     {
+        static IEnumerable<string> ReadData()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "TPChallenge.wordlist";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    yield return reader.ReadLine();
+                }
+            }
+        }
+        
         static void Main(string[] args)
         {
+            var len = ReadData().Count();
+            var a = 3;
         }
     }
 }
